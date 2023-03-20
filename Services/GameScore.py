758cc03b018ba6045e035fee248b211ba16ed4c8
Fredkr0ket <ferdovbalen@gmail.com>
2023-03-20 15:42:23 +0100

    cleaned some stuff up

--- Services/GameScore.py
+++ Services/GameScore.py
@@ -5,38 +5,6 @@ from fastapi import HTTPException
 
 class GameScore:
 
-    def __init__(self, data: GameScore, db: MySQLConnection):
+    def __init__(self, data: GameScore, db: MySQLConnection) -> None:
         self.data = data
-        self.cursor = db.cursor()
-
-    def __del__(self):
-        self.cursor.Close()
-
-    def Get(self) -> GameScore:
-        # GameId
-        pass
-
-    def Upsert(self) -> HTTPException:
-        try:
-            self.cursor.callprocess("", self.data)
-            raise HTTPException(
-                status_code=200,
-                details="Create a new game"
-            )
-        except:
-            raise HTTPException(
-                status_code=500,
-                details="Could not create game"
-            )
-
-    def delete(self) -> HTTPException:
-        try:
-            raise HTTPException(
-                status_code=200,
-                details="Deleted game"
-            )
-        except:
-            raise HTTPException(
-                status_code=500,
-                details="Could not deleted game"
-            )
+        self.db = db
